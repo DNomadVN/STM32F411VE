efcbdee846e277e58ce7591264adfbb75cb70b78
sagittarius-silver <vovandoan4815@gmail.com>
2021-03-23 09:57:55 +0700

    Add detail to TCA9548A Driver

--- MyLibrary/TCA9548A_Driver/TCA9548A_Driver.c
+++ MyLibrary/TCA9548A_Driver/TCA9548A_Driver.c
@@ -1,5 +1,15 @@
 #include "TCA9548A_Driver.h"
 
+
+/*
+ * @brief Initialize
+ * @param htca9548a Pointer to a TCA9548A_HandleTypeDef
+ * @param hi2c Pointer to a I2C_HandleTypeDef structure that contains
+ * 			     the configuration information for the specified I2C.
+ * @param address Target BH1750 device address. The device 7 bits address value in datasheet must
+ *                be shifted to the left before calling by I2C function.
+ * @retval TCA9548A Status
+ */
 TCA9548A_STATUS TCA9548A_Init(TCA9548A_HandleTypeDef *htca9548a, I2C_HandleTypeDef *hi2c, uint16_t address) {
     htca9548a->I2C_channel = hi2c;
     htca9548a->Address = address;
@@ -10,7 +20,12 @@ TCA9548A_STATUS TCA9548A_Init(TCA9548A_HandleTypeDef *htca9548a, I2C_HandleTypeD
     return TCA9548A_ERROR;
 }
 
-
+/*
+ * @brief Initialize
+ * @param htca9548a Pointer to a TCA9548A_HandleTypeDef
+ * @param channel From 0 to 7, is channel 0 to channel 7 on TCA9548A
+ * @retval TCA9548A Status
+ */
 TCA9548A_STATUS TCA9548A_SelectSingleChannel(TCA9548A_HandleTypeDef *htca9548a, uint8_t channel) {
     if (channel > 7) {
         return TCA9548A_ERROR;
@@ -22,7 +37,13 @@ TCA9548A_STATUS TCA9548A_SelectSingleChannel(TCA9548A_HandleTypeDef *htca9548a, 
     return TCA9548A_ERROR;
 }
 
-
+/*
+ * @brief Initialize
+ * @param htca9548a Pointer to a TCA9548A_HandleTypeDef
+ * @param channel This have 8 bit to choose which channel is selected.
+ *                Ex: channel 0, 3, 5 is selected, channel = 0b00101001 = 0x29 
+ * @retval TCA9548A Status
+ */
 TCA9548A_STATUS TCA9548A_SelectMultiChannel(TCA9548A_HandleTypeDef *htca9548a, uint8_t channel) {
     if (HAL_OK == HAL_I2C_Master_Transmit(htca9548a->I2C_channel, htca9548a->Address, &channel, 1, 10)) {
         return TCA9548A_OK;
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # STM32F411VE
 Kit STM32F411VE-DISCO + CubeMX + KeilC
+
+
+## PHASE 1 (Done):
+
+Đọc data cảm biến gửi lên máy tính.
+
+## PHASE 2:
+
+Điều khiển 4 động cơ STEP.
+
+## PHASE 3:
+
+Điều khiển cường độ đèn LED chiếu sáng.
